4b76b69636110726f02d8cb8b86b404e7c3c9fbb
Olly Robinson <ollythedeveloper@gmail.com>
2020-04-09 18:33:45 +0100

    day 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-console.log('hello world')
+import anime from 'animejs/lib/anime.es.js';
+
+const elements = {
+	tunnel: '.tunnel circle',
+	conveyor: '.conveyor-belt .conveyor',
+	zigzag: '.zigzag path',
+	wave: '.wave path',
+}
+
+// tunnel
+anime({
+	targets: elements.tunnel,
+	scale: 1.1,
+	loop: true,
+	direction: 'alternate',
+	duration: 400,
+	easing: 'easeInOutCubic',
+	delay: anime.stagger(80),
+});
+
+// conveyor belt
+anime({
+	targets: elements.conveyor,
+	loop: true,
+	duration: 1500,
+	easing: 'linear',
+	translateX: '-50%',
+})
+
+// zigzag
+anime({
+	targets: elements.zigzag,
+	strokeDashoffset: [anime.setDashoffset, 0],
+	duration: 1500,
+	easing: 'easeInOutSine',
+	direction: 'alternate',
+	loop: true,
+	delay: 800,
+})
+
+// wave
+anime({
+	targets: elements.wave,
+	strokeDashoffset: [anime.setDashoffset, 0],
+	duration: 2000,
+	easing: 'easeInOutCubic',
+	direction: 'alternate',
+	loop: true,
+	delay: 0,
+})
