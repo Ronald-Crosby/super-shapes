42bf216d657051470f132f5e4edd6294e42c1b1c
Olly Robinson <ollythedeveloper@gmail.com>
2020-04-10 20:24:24 +0100

    stuff

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ const elements = {
 	conveyor: '.conveyor-belt .conveyor',
 	zigzag: '.zigzag path',
 	wave: '.wave path',
-	crosses: '.crosses'
+	crosses: '.crosses',
+	squares: '.squares',
+	dots: '.dots'
 }
 
 // selects the first child of an element and duplicates it a given number of times
@@ -16,7 +18,52 @@ const duplicateHTML = (el, quantity) => {
 	elParent.innerHTML = elArray.join('')
 }
 
+// dots
+duplicateHTML(elements.dots, 20)
+
+anime({
+	targets: `${elements.dots} .dot`,
+	scale: [0, 1],
+	delay: anime.stagger(100),
+	loop: true,
+	easing: 'linear',
+	direction: 'alternate',
+	autoplay: true,
+	duration: 250,
+})
+
+// squares
+duplicateHTML(elements.squares, 40)
+
+anime({
+	targets: `${elements.squares} .square`,
+	rotate: () => {
+		return anime.random(0, 360)
+	},
+	duration: () => {
+		return anime.random(500, 1500)
+	},
+	translateX: () => {
+		return anime.random(0, 8)
+	},
+	loop: true,
+	easing: 'easeInOutSine',
+	direction: 'alternate',
+	autoplay: true,
+})
+
+// crosses
 duplicateHTML(elements.crosses, 10)
+
+anime({
+	targets: `${elements.crosses} path`,
+	loop: true,
+	duration: 1000,
+	direction: 'alternate',
+	easing: 'easeInOutSine',
+	rotate: 360,
+	delay: anime.stagger(90)
+})
 
 // tunnel
 anime({
